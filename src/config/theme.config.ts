1f1039fde4a661d9542a7346800cc94f13d33f20
boroicaradu <boroicaradu.dev@gmail.com>
2026-07-29 21:16:30 +0300

    Update theme.config.ts

--- src/config/theme.config.ts
+++ src/config/theme.config.ts
@@ -5,7 +5,7 @@ const siteUrl = (
 ).replace(/\/$/, "");
 
 export const SITE = {
-  name: "Betel",
+  name: "Biserica Penticostală BETEL",
   description:
     "un singur trup, un singur Duh, un singur Domn, o singură credință, un singur botez, un singur Dumnezeu şi Tată al tuturor",
   url: siteUrl,
