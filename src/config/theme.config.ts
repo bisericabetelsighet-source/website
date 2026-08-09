46c1ed49026e86e8e2c41687c4a9072e4ecc4aa9
paulieudean02 <paulieudean@gmail.com>
2026-08-09 09:40:09 +0300

    Update theme.config.ts

--- src/config/theme.config.ts
+++ src/config/theme.config.ts
@@ -5,7 +5,7 @@ const siteUrl = (
 ).replace(/\/$/, "");
 
 export const SITE = {
-  name: "BETEL",
+  name: "Biserica Penticostală Betel Sighetu Marmației",
   description:
     "un singur trup, un singur Duh, un singur Domn, o singură credință, un singur botez, un singur Dumnezeu şi Tată al tuturor",
   url: siteUrl,
